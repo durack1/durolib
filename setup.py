148ed2138fd6b1c1627c7e8aaf6cd4ddcfa8b5d1
Paul J. Durack <durack1@llnl.gov>
2019-03-13 13:26:42 -0700

    Correct author-email declaration

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 # Set package version
-Version="1.1.2"
+Version='1.1.2'
 
 # Call setup function - see https://setuptools.readthedocs.io/en/latest/setuptools.html#adding-setup-arguments
 setup(
-      name = "durolib",
-      author = "Paul J. Durack",
-      author-email = "durack1@llnl.gov",
+      name = 'durolib',
+      author = 'Paul J. Durack',
+      author_email = 'durack1@llnl.gov',
       data_files = [('share/durolib/data',['data/CMIP5BranchTimes.json',
                                            'data/CMIP5BranchTimes.pickle'])],
-      description = "Python utilities for climate",
+      description = 'Python utilities for climate',
       packages = ['durolib'],
-      url = "http://github.com/durack1/durolib",
-      version=Version,
+      url = 'http://github.com/durack1/durolib',
+      version = Version,
       )
 
 #      package_dir = {'durolib': 'lib'},
